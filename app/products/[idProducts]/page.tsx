662002f050173121ccc106752448009fa8d82d76
quynh410 <quynhhhhhh07@gmail.com>
2024-09-04 15:52:07 +0700

    add

--- app/products/[idProducts]/page.tsx
+++ app/products/[idProducts]/page.tsx
@@ -1,17 +1,13 @@
-"use client"
-import { useSearchParams } from 'next/navigation'
-import React from 'react'
+"use client";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 export default function Page() {
-    // lấy query bên cạnh dấu ? 
-    // Dùng useSearchParams đây là hook được cung cấp bởi next js
-    const searchParams = useSearchParams();
-    const idProduct = searchParams.get("idProducts")
-    console.log(idProduct);
-    
-  return (
-    <div>
-      Chi tiết sản phẩm
-    </div>
-  )
+  // lấy query bên cạnh dấu ?
+  // Dùng useSearchParams đây là hook được cung cấp bởi next js
+  const searchParams = useSearchParams();
+  const idProduct = searchParams.get("idProducts");
+  console.log(idProduct);
+
+  return <div>Chi tiết sản phẩm</div>;
 }
